3abd0f50c7eef087797bd447430e5712626c6334
demidraksin <greatraksin@icloud.com>
2021-06-28 11:15:29 +0300

    ⭐️Adding parser starter project⭐️

--- 02_parser_2806/my_parser.py
+++ 02_parser_2806/my_parser.py
@@ -4,13 +4,17 @@ import pandas as pd
 def parse_dominos(card):
     res = pd.DataFrame()  # таблица для результатов
     url = 'https://dominos.by/pizza/'
-    pizza_link = url + card.get('data-code')  # ссылка на пиццу
-    print(pizza_link,)
-    '''
+    pizza_link = url + card.get('data-code').lower()  # ссылка на пиццу
+    pizza_name = card.find('div', {'class': 'product-card__title'}).text
+    pizza_ingr = card.find('div', {'class': 'product-card__description'}).text
+    pizza_img = card.find('img', {'class': 'product-card-media__element'}).get('src')
+    pizza_price = card.find('p', {'class': 'product-card__modification-info-price'}).text
+    print(pizza_link, pizza_name, pizza_ingr, pizza_img, pizza_price)
+
     res = res.append(
         pd.DataFrame(  # создаю таблицу на основе полученных данных
-            [[]],
-            columns=['Ссылка', 'Название', 'Ингридиенты', 'Цена', 'Масса', 'картинка']),
+            [[pizza_link, pizza_name, pizza_ingr, pizza_price, pizza_img]],
+            columns=['Ссылка', 'Название', 'Ингредиенты', 'Цена', 'картинка']),
         ignore_index=True
-    )'''
+    )
     return res
