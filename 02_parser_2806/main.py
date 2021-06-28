baec2e59ca925d5b84537fed48f2071dfe9ba210
demidraksin <greatraksin@icloud.com>
2021-06-28 09:46:55 +0300

    ⭐️Adding parser starter project⭐️

--- 02_parser_2806/main.py
+++ 02_parser_2806/main.py
@@ -4,14 +4,15 @@ from bs4 import BeautifulSoup
 from my_parser import *
 # ^ это мой файл, в котором будет функция парсинга
 
-url = ''
+url = 'https://dominos.by/pizza'
 connect = requests.get(url)
 df = pd.DataFrame()
 soup = BeautifulSoup(connect.text, features='html.parser')
-cards = ''
+cards = soup.find_all('div', {'class': 'product-card'})
 
 for item in cards:
     s = parse_dominos(item)
     df = df.append(s, ignore_index=True)
+    #print(item)
 
 df.to_excel('dominos.xlsx')
--- 02_parser_2806/my_parser.py
+++ 02_parser_2806/my_parser.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 def parse_dominos(card):
     res = pd.DataFrame()  # таблица для результатов
+    url = 'https://dominos.by/pizza/'
+    pizza_link = url + card.get('data-code')  # ссылка на пиццу
+    print(pizza_link,)
     '''
     res = res.append(
         pd.DataFrame(  # создаю таблицу на основе полученных данных
